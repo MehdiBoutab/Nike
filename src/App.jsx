a23d220777e6bbaeb65a9d56cb012688f3577695
Mehdi Boutab <mehdiboutab@gmail.com>
2023-12-13 17:25:41 +0100

    Adding Nav logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
+import Hero from './sections/Hero'
+import CustomerReviews from './sections/CustomerReviews'
+import Footer from './sections/Footer'
+import Nav from './components/Nav'
+import PopularProducts from './sections/PopularProducts'
+import Services from './sections/Services'
+import SpecialOffer from './sections/SpecialOffer'
+import Subscribe from './sections/Subscribe'
+import SuperQuality from './sections/SuperQuality'
+
 export default function App() {
   return (
     <main className="relative">
-      Nav
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero />
       </section>
       <section className="padding">
-        PopularProducts
+        <PopularProducts />
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services />
       </section>
       <section className="padding">
-        SpecialOffer
+        <SpecialOffer />
       </section>
       <section className="bg-pale-blue padding">
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className="padding padding-x sm:py-32 py-16">
-        Subscribe
+        <Subscribe />
       </section>
       <section className="padding-xpadding-t pb-8 bg-black">
-        Footer
+        <Footer />
       </section>
     </main>
   )
