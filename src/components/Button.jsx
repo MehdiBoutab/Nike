8b990baf526b0e398c1d9bc06f341a62b0922893
Mehdi Boutab <mehdiboutab@gmail.com>
2023-12-19 15:53:41 +0100

    Implement SpecialOffer component with detailed content and call-to-action buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
+  const backgroundCOlor = backgroundColor ? backgroundColor : 'bg-coral-red';
+  const textCOlor = textColor ? textColor : 'text-white';
+  const borderCOlor = borderColor ? borderColor: 'border-coral-red';
+
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+    ${backgroundCOlor} ${textCOlor} ${borderCOlor} rounded-full`}>
       {label}
       {iconURL && <img src={iconURL} alt="Arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
