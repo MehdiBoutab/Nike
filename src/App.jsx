559d74235be3a7b69d14c7745ef88d86cbae75fb
Mehdi Boutab <mehdiboutab@gmail.com>
2023-12-19 16:45:27 +0100

    🌟 Implemented a customized footer with terms and conditions link 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,30 @@ import SuperQuality from './sections/SuperQuality'
 
 export default function App() {
   return (
-    <main className="relative">
+    <main className='relative'>
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <PopularProducts />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="bg-pale-blue padding">
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
-      <section className="padding padding-x sm:py-32 py-16">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="padding-xpadding-t pb-8 bg-black">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
